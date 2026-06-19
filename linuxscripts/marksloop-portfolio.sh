b8c20ba85b537d77b738cedee74edf15e2aa9482
frothywaffler <coltoneller056@gmail.com>
2026-06-19 00:41:38 -0500

    Update marksloop-portfolio.sh

--- linuxscripts/marksloop-portfolio.sh
+++ linuxscripts/marksloop-portfolio.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# Path of Titans Marks Automation
+# Portfolio-safe example version
+
+while true
+do
+for i in {1..3}; do
+rconclt pot setmarksall 100000
+sleep 0.5
+done
+
+sleep 30
+done
